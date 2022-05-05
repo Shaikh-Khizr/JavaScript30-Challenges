cf1120248a3d336af663352fa2c0ac157ef518f1
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2022-05-05 15:48:29 +0530

    Countdown Timer Done - 29

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -1,7 +1,7 @@
 let countdown;
-const timerDisplay = document.querySelector('.display__time-left');
-const endTime = document.querySelector('.display__end-time');
-const buttons = document.querySelectorAll('[data-time]');
+const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
+const buttons = document.querySelectorAll("[data-time]");
 
 function timer(seconds) {
   // clear any existing timers
@@ -12,13 +12,14 @@ function timer(seconds) {
   displayTimeLeft(seconds);
   displayEndTime(then);
 
-  countdown = setInterval(() => {
+  countdown = setInterval(function () {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
-    // check if we should stop it!
-    if(secondsLeft < 0) {
+    // check if we sould stop it
+    if (secondsLeft < 0) {
       clearInterval(countdown);
       return;
     }
+
     // display it
     displayTimeLeft(secondsLeft);
   }, 1000);
@@ -27,7 +28,7 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
-  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
   document.title = display;
   timerDisplay.textContent = display;
 }
@@ -37,7 +38,7 @@ function displayEndTime(timestamp) {
   const hour = end.getHours();
   const adjustedHour = hour > 12 ? hour - 12 : hour;
   const minutes = end.getMinutes();
-  endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+  endTime.textContent = `${adjustedHour}:${minutes < 10 ? "0" : ""}${minutes}`;
 }
 
 function startTimer() {
@@ -45,8 +46,8 @@ function startTimer() {
   timer(seconds);
 }
 
-buttons.forEach(button => button.addEventListener('click', startTimer));
-document.customForm.addEventListener('submit', function(e) {
+buttons.forEach((button) => button.addEventListener("click", startTimer));
+document.customForm.addEventListener("submit", function (e) {
   e.preventDefault();
   const mins = this.minutes.value;
   console.log(mins);
